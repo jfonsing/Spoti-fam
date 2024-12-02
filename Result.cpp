04674bec5a50328d79f18070c691e19c6a714728
timothychoy <tjc.choy@gmail.com>
2024-12-02 13:08:07 -0500

    result page

---
+++ Result.cpp
@@ -0,0 +1,122 @@
+//
+// Created by Timothy Choy on 12/2/24.
+//
+#include <SFML/Graphics.hpp>
+#include "Result.h"
+using namespace std;
+
+bool Results::createWindowR(){
+    sf::Font font;
+    if (!font.loadFromFile("images/font.ttf"))
+    {
+        std::cerr << "Error loading font" << std::endl;
+        return -1;  // Exit on error
+    }
+    //JENNAS SHIT
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Results Screen", sf::Style::Fullscreen);
+    sf::Texture logo;
+    if (!logo.loadFromFile("images/logo.png")) {
+        return false;
+    }
+
+    sf::Sprite showLogo;
+    showLogo.setTexture(logo);
+
+
+    sf::FloatRect logoBounds = showLogo.getLocalBounds();
+    showLogo.setOrigin(logoBounds.width / 2.f, logoBounds.height / 2.f);
+
+
+    showLogo.setScale(0.50f, 0.50f);
+    showLogo.setPosition(window.getSize().x / 2.f, (showLogo.getLocalBounds().height / 2.f)-150);
+
+    sf::Texture logout;
+    if (!logout.loadFromFile("images/logout.png")) {
+        return false;
+    }
+    sf::Sprite showLogout;
+    showLogout.setTexture(logout);
+    showLogout.setScale(0.30,0.30);
+
+    showLogout.setPosition(0.f, (showLogo.getLocalBounds().height / 2.f)-150);
+
+    sf::Texture profile;
+    if (!profile.loadFromFile("images/profile.png")) {
+        return false;
+    }
+    sf::Sprite showProfile;
+    showProfile.setTexture(profile);
+    showProfile.setScale(0.50,0.50);
+    showProfile.setPosition(window.getSize().x - showLogo.getLocalBounds().width * 0.50f, 10.f);
+    // my shit
+    sf::Text recommendationText;
+    recommendationText.setFont(font);
+    recommendationText.setString("Your daily song recommendation is: ");
+    recommendationText.setCharacterSize(18);
+    recommendationText.setFillColor(sf::Color::White);
+    recommendationText.setStyle(sf::Text::Bold);
+
+    sf::FloatRect textBounds = recommendationText.getLocalBounds();
+    recommendationText.setOrigin(textBounds.width / 2.f, textBounds.height / 2.f);
+    recommendationText.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f - 100);
+
+    sf::Text songText;
+    songText.setFont(font);
+    songText.setString("THIS IS THE SONG(REPLACE)");
+    songText.setCharacterSize(18);
+    songText.setFillColor(sf::Color::White);
+    songText.setStyle(sf::Text::Bold);
+
+    sf::FloatRect songBounds = songText.getLocalBounds();
+    songText.setOrigin(songBounds.width / 2.f, songBounds.height / 2.f);
+    songText.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f - 50);
+
+
+    sf::Text recText;
+    recText.setFont(font);
+    recText.setString("Your recommended friend is:");
+    recText.setCharacterSize(18);
+    recText.setFillColor(sf::Color::White);
+    recText.setStyle(sf::Text::Bold);
+
+    sf::FloatRect recBounds = recText.getLocalBounds();
+    recText.setOrigin(recBounds.width / 2.f, recBounds.height / 2.f);
+    recText.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+
+    sf::Text friendText;
+    friendText.setFont(font);
+    friendText.setString("ACTUAL FRIEND (REPALCE WITH STRING)");
+    friendText.setCharacterSize(18);
+    friendText.setFillColor(sf::Color::White);
+    friendText.setStyle(sf::Text::Bold);
+
+    sf::FloatRect friendBounds = friendText.getLocalBounds();
+    friendText.setOrigin(friendBounds.width / 2.f, friendBounds.height / 2.f);
+    friendText.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f + 50);
+
+
+    //jenna's shit
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
+        if (window.isOpen()) {
+            window.clear(sf::Color(128, 128, 128));
+            window.draw(showLogo);
+            window.draw(showLogout);
+            window.draw(showProfile);
+            // added from me
+            window.draw(recommendationText);
+            window.draw(songText);
+            window.draw(recText);
+            window.draw(friendText);
+            window.display();
+        }
+    }
+    return false;
+
+}
---
+++ Result.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <SFML/Graphics.hpp>
+using namespace std;
+#pragma once
+//
+class Results{
+public:
+    bool createWindowR();
+
+private:
+    sf::Sprite sprite;
+};
